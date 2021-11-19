9d6e74143dd8ff19d8ec131bee35756a731a262d
Mitsuharu Emoto <mthr1982@gmail.com>
2021-11-19 21:37:53 +0900

    typo

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ echo "macOSの開発初期設定を行います"
 echo "================================"
 
 # clone
-echo "(1) GitHubからレポジトリをダウンロードしています"
+echo "(A) GitHubからレポジトリをダウンロードしています"
 # git clone https://github.com/mitsuharu/macos-infra.git ${DIR}
 
 # install
-echo "(2) 開発初期設定を開始します\n"
+echo "(B) 開発初期設定を開始します\n"
 ${DIR}/shells/init.sh
