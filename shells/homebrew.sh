0b461b5a595a68a133137e36ddcd8768d8ccff1e
Mitsuharu Emoto <mthr1982@gmail.com>
2022-01-09 10:34:57 +0900

    add java at brew

--- shells/homebrew.sh
+++ shells/homebrew.sh
@@ -7,4 +7,7 @@ echo "Homebrew をインストールします"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle
 
+# java のシンボリック
+sudo ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
 source ~/.zprofile
