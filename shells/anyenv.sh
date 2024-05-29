757fcd2f0be58e48c141ceb8c8cfb33676f4d5dd
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2024-05-29 09:26:24 +0900

    add rbenv-aliases

--- shells/anyenv.sh
+++ shells/anyenv.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-RUBY_VER="3.2.3"
-NODE_VER="20.11.1"
+RUBY_VER="3.2.4"
+NODE_VER="20.13.1"
 
 source ~/.zprofile
+wait
 
 # anyenv
 echo "anyenv を設定します"
@@ -11,6 +12,7 @@ anyenv init
 anyenv install --init
 
 source ~/.zprofile
+wait
 
 # anyenv update
 echo "anyenv update をダウンロードしています"
@@ -25,6 +27,7 @@ echo "nodenv をインストールします"
 anyenv install nodenv
 
 source ~/.zprofile
+wait
 
 echo "ruby ${RUBY_VER} をインストールします"
 rbenv install ${RUBY_VER}
@@ -38,6 +41,10 @@ nodenv global ${NODE_VER}
 
 source ~/.zprofile
 wait
+
+echo "rbenv-aliases をインストールします"
+git clone https://github.com/tpope/rbenv-aliases.git $(rbenv root)/plugins/rbenv-aliases
+rbenv alias --auto
 
 echo "nodenv-aliases をインストールします"
 git clone https://github.com/nodenv/nodenv-aliases.git $(nodenv root)/plugins/nodenv-aliases
